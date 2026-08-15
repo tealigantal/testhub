5b2a0dc9ebc8d1edda5016db603a28639a19a385
tealigantal <tealigantal@gmail.com>
2026-08-16 05:16:44 +0800

    Add Perfect Player activity

---
+++ android-perfect-player/app/src/main/java/com/tealigantal/perfectplayer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.tealigantal.perfectplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        if (savedInstanceState == null) webView.loadUrl("file:///android_asset/index.html");
+        else webView.restoreState(savedInstanceState);
+    }
+    @Override protected void onSaveInstanceState(Bundle outState) { webView.saveState(outState); super.onSaveInstanceState(outState); }
+    @Override public void onBackPressed() { if (webView != null && webView.canGoBack()) webView.goBack(); else super.onBackPressed(); }
+    @Override protected void onDestroy() { if (webView != null) webView.destroy(); super.onDestroy(); }
+}
